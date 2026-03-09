2902781fcd39d55ea5aa0bcb850a0d1e61907626
JuanaVillalba <juananicolvillalba@gmail.com>
2026-03-09 14:31:28 -0300

    Version estilizada de la card

--- js/index.js
+++ js/index.js
@@ -1,8 +1,33 @@
 const valores=["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
 
-function recorrerValores(valores){
+function recorrerValores(valor){
+    let aleatorio= Math.floor(Math.random()*valor.length);
     let h1 = document.querySelector("h1");
-    if (h1.textContent===""){
-        h1 = valores[Math.floor(Math.random()* valores.length)];
-    }
+    h1.textContent=valor[aleatorio];
 }
+function recorrerPalos(simpolo){
+    let random= Math.floor(Math.random()*simpolo.length);
+    document.getElementById("imagen").src = simpolo[random].img;
+    document.getElementById("imagen2").src = simpolo[random].img;
+}
+
+const palos=[{
+        titulo: "pica",
+        img: "assets/img/pica.png"
+    },
+    {
+        titulo: "trebol",
+        img: "assets/img/trebol.png"
+    },
+    {
+        titulo: "diamante",
+        img: "assets/img/diamante.png"
+    },{
+        titulo: "corazon",
+        img: "assets/img/corazon.png",
+        
+    }];
+
+recorrerValores(valores);
+recorrerPalos(palos);
+
